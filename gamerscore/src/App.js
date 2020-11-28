5ba92b84e3c45e493d0e8025a37c633cdfbab02e
corey-power <CPower1248@gmail.com>
2020-11-28 18:01:11 -0500

    Refactor to components, added Input(no-func)

--- gamerscore/src/App.js
+++ gamerscore/src/App.js
@@ -1,41 +1,45 @@
+import React, { useState } from "react"
 import './App.css';
+import Console from "./components/Console"
+
+const initialScores = [
+  {
+    name: "Playstation",
+    score: 255
+  },
+  {
+    name: "Xbox",
+    score: 27
+  },
+  {
+    name: "Nintendo",
+    score: 64
+  },
+  {
+    name: "Steam",
+    score: 377
+  }
+];
+const initialGE = 181;
 
 function App() {
+  const [ scores, setScores ] = useState(initialScores)
+  const [ ge, setGE ] = useState(initialGE)
+
   return (
     <div className="App">
       <h1>GamerscorE</h1>
       <div className="scores-container">
         <div className="console-container">
-          <div className="console-playstation">
-            <h3>Playstation</h3>
-            <div className="console-score-playstation">
-              <span>276</span>
-            </div>
-          </div>
-          <div className="console-xbox">
-            <h3>X-Box</h3>
-            <div className="console-score-xbox">
-              <span>78</span>
-            </div>
-          </div>
-          <div className="console-nintendo">
-            <h3>Nintendo</h3>
-            <div className="console-score-nintendo">
-              <span>24</span>
-            </div>
-          </div>
-          <div className="console-steam">
-            <h3>Steam</h3>
-            <div className="console-score-steam">
-              <span>377</span>
-            </div>
-          </div>
+          {scores.map(item => {
+            return <Console console={item} />
+          })}
         </div>
         <div className="ge-container">
           <div className="console-ge">
             <h3>GE Score</h3>
             <div className="console-score-ge">
-              <span>189</span>
+              <span>{ge}</span>
             </div>
           </div>
         </div>
---
+++ gamerscore/src/scratchpad.js
@@ -0,0 +1,24 @@
+{/* <div className="console-playstation">
+<h3>Playstation</h3>
+<div className="console-score-playstation">
+  <span>{scores.playstation}</span>
+</div>
+</div>
+<div className="console-xbox">
+<h3>X-Box</h3>
+<div className="console-score-xbox">
+  <span>{scores.xbox}</span>
+</div>
+</div>
+<div className="console-nintendo">
+<h3>Nintendo</h3>
+<div className="console-score-nintendo">
+  <span>{scores.nintendo}</span>
+</div>
+</div>
+<div className="console-steam">
+<h3>Steam</h3>
+<div className="console-score-steam">
+  <span>{scores.steam}</span>
+</div>
+</div> */}
